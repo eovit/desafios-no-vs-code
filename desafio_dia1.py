514c48ac9f34ef5547d0f5e60f8cf9a6f6a1e259
vito <vitorluciano1806@gmail.com>
2026-03-03 05:47:27 -0300

    Add files via upload

---
+++ desafio_dia1.py
@@ -0,0 +1,12 @@
+#facinho mlkd
+
+#começo o código fazendo a máquina te pedir um número, e determinando a variável número
+numero = (float(input("Digite qualquer número, sério, qualquer um: ")))
+
+#aqui eu to explicando pra máquina o que ela tem que fazer em cada caso, igual na primeira e segunda aula do Luiz
+if numero > 10:
+    print("Seu número é maior que 10, parabéns!")
+elif numero < 10:
+    print("Seu número é menor que 10 tadinho dele ;-;")
+else:
+    print("Seu número é 10!!!") #eu decidi colocar uma condição a mais só pra brincar um pouquinho, tmj >.<
